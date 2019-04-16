f2e5445e4e124af86f1a73e9c68a2e7eec8ad75b
MichaelRomero <liikemike@aim.com>
2019-04-16 19:15:47 -0400

    added conversations and messages model, channel, serializer, controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users, :messages, :conversations
       resources :posts
 
       post '/login', to: 'auth#create'
       get '/current_user', to: 'auth#show'
+      mount ActionCable.server => '/cable'
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_13_190439) do
+ActiveRecord::Schema.define(version: 2019_04_16_223251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "text"
+    t.bigint "conversation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
@@ -33,4 +47,5 @@ ActiveRecord::Schema.define(version: 2019_04_13_190439) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "conversations"
 end
