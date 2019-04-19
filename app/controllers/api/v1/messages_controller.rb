63e4351858cd45b16d375e7393348b14e3598857
Maximo Bautista <max.bautista97@gmail.com>
2019-04-19 12:13:14 -0400

    Fixed error on message controller when creating the message that stopped the message from being broadcasted.

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,10 +1,9 @@
 class Api::V1::MessagesController < ApplicationController
+
   def create
-    @user = User.find(message_params[:user_id])
     @message = Message.new(message_params)
-    @message.user = @user
     @conversation = Conversation.find(message_params[:conversation_id])
-    # byebug
+
     if @message.save
       serialized_data = ActiveModelSerializers::Adapter::Json.new(
         MessageSerializer.new(@message)
